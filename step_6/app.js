31f36d16f01c594d47fbd662557509d9d8d1598b
sonagara-vashram <vashramsonagara0106@gmail.com>
2024-10-14 18:09:10 +0530

    node.js global object

---
+++ step_6/app.js
@@ -0,0 +1,8 @@
+require('./global_var'); // file1.js ko import kiya
+
+console.log(myVar); // ✅ Output: I am globally accessible!
+
+
+greet('John'); // ✅ Output: Hello, John!
+
+
---
+++ step_6/global_var.js
@@ -0,0 +1,7 @@
+global.myVar = "I am globally accessible!"; // Global variable
+// accessible in anoher files
+
+// Global function
+global.greet = function (name) {
+    console.log(`Hello, ${name}!`);
+};
